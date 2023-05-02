858d2915a7e900934e6b29a8a3f67a118f198e48
erick <erick@ciencias.unam.mx>
2023-05-02 15:50:45 -0600

    y mueve y mueve y mueve el eskeleto y mueve al baila

---
+++ Bug/Assets/Scripts/Personajes/Abeja.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abeja : Enemigos
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Atacar(){
+        Debug.Log("me quiero morir");
+    }
+
+}
---
+++ Bug/Assets/Scripts/Personajes/Jugador.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    int Vida;
+    int Olor;
+    int SaltoActual;
+    int SaltoMaximo;
+    bool EscudoDisponible;
+    bool LlamadoDisponible;
+    bool DobleSaltoDisponible;
+    bool HormonasDisponible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Habilidad()
+    {
+
+    }
+
+    void Movimiiento()
+    {
+
+    }
+
+    void RecargaEscudo()
+    {
+
+    }
+
+    void RecargaOlor()
+    {
+
+    }
+}
