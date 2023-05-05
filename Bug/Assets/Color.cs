559637cc370e085da98bb11a7cdec7808920d964
rubenAcostaArzate <rubenacostaarzate@ciencias.unam.mx>
2023-05-05 03:31:47 -0600

    Primer avance

---
+++ Bug/Assets/Color.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour
+{
+    public Color color=new Color(0.2F,0.3F,0.4F,0.5F);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
