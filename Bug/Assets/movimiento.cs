7d81a15a8da314a5915cf53b591a255bef7ab80e
rubenAcostaArzate <rubenacostaarzate@ciencias.unam.mx>
2023-05-07 03:53:05 -0600

    agregando animaciones de movimiento

--- Bug/Assets/movimiento.cs
+++ Bug/Assets/movimiento.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class movimiento : MonoBehaviour
 {
-private Rigidbody _rb;
+    private Rigidbody _rb;
+
+    private Animator animador;
 
     public float velocidadSaltoInicial;
 
@@ -17,30 +19,39 @@ private Rigidbody _rb;
     public SpriteRenderer sprite;
 
     public Transform deteccionSuelo;
+
+
     // Start is called before the first frame update
     void Start() {
         _rb = GetComponent<Rigidbody>();
-
+        animador=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
+
+        animador.SetBool("se_mueve",false);
         if(Physics2D.CircleCast(deteccionSuelo.position,0.1f,Vector2.zero)) {
             enSuelo = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && enSuelo) {
+            animador.SetBool("se_mueve",true);
             _rb.velocity = new Vector3(_rb.velocity.x,velocidadSaltoInicial);
         } else if (Input.GetKey(KeyCode.A)) {
             sprite.flipX=true;
+            animador.SetBool("se_mueve",true);
             transform.Translate(0.1f,0f,0f);
         } else if (Input.GetKey(KeyCode.D)) {
             sprite.flipX=false;
+            animador.SetBool("se_mueve",true);
             transform.Translate(-0.1f,0f,0f);
         } else if (Input.GetKey(KeyCode.W)){
+            animador.SetBool("se_mueve",true);
             transform.Translate(0f,0f,0.1f);
 
         }else if (Input.GetKey(KeyCode.S)) {
+            animador.SetBool("se_mueve",true);
             transform.Translate(0f,-0f,-0.1f);
         }
 
